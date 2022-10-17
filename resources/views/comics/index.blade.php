1755bac137a4d06b692e57d6a5161b6ae0c9ae60
MaryChiodelli <marychiodelli97@gmail.com>
2022-10-17 18:26:12 +0200

    css changes in header footer and comics

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -11,16 +11,18 @@
     <ul class="container comics-wrapper">
         @forelse($comics as $comic)
         <li class="card">
-            <a class="card-link" href="{{ route('comics.show', $comic) }}">
+            <a class="card-content" href="{{ route('comics.show', $comic) }}">
                 <img src="{{ $comic->thumb }}" alt="{{ $comic->title }}">
                 <h3>{{ $comic->title }}</h3>
 
-                <a class="btn" href="{{ route('comics.edit', $comic) }}">Edit</a>
-                <form action="{{ route('comics.destroy', $comic) }}" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <input class="btn" type="submit" value="Delete">
-                </form>
+                <div class="card-footer">
+                    <a class="btn" href="{{ route('comics.edit', $comic) }}">Edit</a>
+                    <form action="{{ route('comics.destroy', $comic) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <input class="btn delete" type="submit" value="Delete">
+                    </form>
+                </div>
             </a>
         </li>
         @empty
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,16 +1,18 @@
 <header>
-    <nav>
-        <ul>
-            <li>
-                <a href="{{ route('home') }}">
-                    Home
-                </a>
-            </li>
-            <li>
-                <a href="{{ route('comics.index') }}">
-                    Comics
-                </a>
-            </li>
-        </ul>
-    </nav>
+    <div class="container">
+        <nav>
+            <ul class="nav">
+                <li>
+                    <a href="{{ route('home') }}">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a href="{{ route('comics.index') }}">
+                        Comics
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </header>
